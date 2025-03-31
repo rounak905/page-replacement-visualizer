88df5c474ca26d280bea571713928e6b5b57d32a
kartikkk7777 <kartikkumarsingh699@gmail.com>
2025-03-31 19:22:18 +0530

    Implemented LRU page replacement algorithm

--- src/page_algoriths.py
+++ src/page_algoriths.py
@@ -19,3 +19,21 @@ class PageReplacement:
             print(f"Page: {page} | Frames: {self.frames}")
 
         return page_faults
+    
+    def lru(self, pages):
+        """ Least Recently Used (LRU) Page Replacement Algorithm """
+        page_faults = 0
+        self.frames = []
+        
+        for page in pages:
+            if page in self.frames:
+                self.frames.remove(page)
+            elif len(self.frames) >= self.capacity:
+                self.frames.pop(0)
+                page_faults += 1
+            else:
+                page_faults += 1
+            self.frames.append(page)
+            print(f"Page: {page} | Frames: {self.frames}")
+            
+        return page_faults
